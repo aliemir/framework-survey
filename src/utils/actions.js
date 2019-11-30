766833924919e755412e45da252c3d51765c152e
Ali Emir Şen <senaliemir@gmail.com>
2019-11-30 16:01:18 +0300

    actions and states

---
+++ src/utils/actions.js
@@ -0,0 +1,9 @@
+const actions = {
+  SET_ANSWER: "SET_ANSWER",
+  SET_CONTEXT: "SET_CONTEXT",
+  NEXT: "NEXT",
+  PREV: "PREV",
+  SET_COMMENT: "SET_COMMENT"
+};
+
+export default actions;
---
+++ src/utils/states.js
@@ -0,0 +1,9 @@
+const states = {
+  IDLE: "IDLE",
+  CONTEXT_SELECT: "CONTEXT_SELECT",
+  QUESTION: "QUESTION",
+  COMMENT: "COMMENT",
+  END: "END"
+};
+
+export default states;
